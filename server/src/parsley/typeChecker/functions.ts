dd399058787a16f128f591cdd5c6e6a6e47f1d5e
Benjamin J. Cape <benjamin.j.cape.22@dartmouth.edu>
2022-02-16 18:28:49 -0500

    update for new json output

--- server/src/parsley/typeChecker/functions.ts
+++ server/src/parsley/typeChecker/functions.ts
@@ -1,7 +1,37 @@
 import * as child from "child_process";
 import * as url from "url";
-import { Diagnostic, _Connection } from "vscode-languageserver";
+import {
+  Diagnostic,
+  Position,
+  Range,
+  _Connection,
+} from "vscode-languageserver";
 import { TextDocument } from "vscode-languageserver-textdocument";
+
+const GHOST_RANGE: Range = {
+  start: {
+    character: 0,
+    line: 0,
+  },
+  end: {
+    character: 1,
+    line: 0,
+  },
+};
+
+type ErrLocation = {
+  fname: string;
+  lnum: number;
+  bol: number;
+  cnum: number;
+};
+
+type ErrMessage = {
+  errm_start: ErrLocation;
+  errm_end: ErrLocation;
+  errm_ghost: boolean;
+  errm_reason: string;
+};
 
 async function executeTypeChecker(
   filename: string
@@ -16,28 +46,29 @@ async function executeTypeChecker(
   });
 }
 
+function convertToPosition(errLocation: ErrLocation): Position {
+  return {
+    character: errLocation.cnum - errLocation.bol,
+    line: errLocation.lnum - 1,
+  };
+}
+
 function parseParsleyResponse(response: string): Diagnostic | undefined {
   const data = response.split("\n")[1];
-  const error = JSON.parse(data);
+  const error: ErrMessage = JSON.parse(data);
 
   if (error) {
-    const { lnum, bol, cnum } = error;
+    const { errm_start, errm_end, errm_reason, errm_ghost } = error;
 
-    const character = cnum - bol;
-
-    return {
-      message: response,
-      range: {
-        start: {
-          character,
-          line: lnum - 2,
-        },
-        end: {
-          character: character + 1,
-          line: lnum - 2,
-        },
-      },
-    };
+    return errm_ghost
+      ? { message: errm_reason, range: GHOST_RANGE }
+      : {
+          message: errm_reason,
+          range: {
+            start: convertToPosition(errm_start),
+            end: convertToPosition(errm_end),
+          },
+        };
   }
 }
 
